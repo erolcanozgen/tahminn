13b87be0537da490f85d9cd10c97318e6c5d00dd
ibrahim <ibrahimsckn35@gmail.com>
2019-05-11 23:38:17 +0300

    commit

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/tahminn'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname,'/dist/tahminn/index.html'));
+app.get('/*', function (req, res) {
+
+    res.sendFile(path.join(__dirname, '/dist/tahminn/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8090);
