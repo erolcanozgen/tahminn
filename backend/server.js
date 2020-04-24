14ab6490915828941839e225699978424ba149f7
Erol Can OZGEN <erolcanozgen@gmail.com>
2020-04-24 22:32:34 +0300

    unnecessary actions to copy build folder to backend directorey is removed.

--- backend/server.js
+++ backend/server.js
@@ -45,9 +45,9 @@ app.set('io', io)
 app.use('/', authRouter)
 
 
-app.use(express.static(path.join(__dirname, 'build')));
-app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.use(express.static(path.join(__dirname, '/../client/build')));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/../client/build', 'index.html'));
   });
 
 server.listen(process.env.PORT || 8080, () => {
--- ops/copyBuildFolder.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs-extra');
-
-fs.copy('./client/build/', './backend/build/', err =>{
-  if(err) return console.error(err);
-  console.log('success!');
-});
