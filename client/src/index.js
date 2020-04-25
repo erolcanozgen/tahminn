223f78ae1526207d7bfdf049ec56f4b4f7b58b12
ibrahim <ibrahimsckn35@gmail.com>
2020-04-25 18:38:08 +0300

    Unused app folder is removed. React router dom is added for initial purposes.

--- client/src/index.js
+++ client/src/index.js
@@ -2,12 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './assets/styles/index.css'
 import Login from './components/login/login'
+import Home from './components/home/home'
 import registerServiceWorker from './registerServiceWorker'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.min.js'
 import $ from 'jquery';
 import Popper from 'popper.js';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-ReactDOM.render(<Login />, document.getElementById('root'))
+
+ReactDOM.render((
+    <Router>
+        <Route path="/" component={Login} />
+    </Router>
+), document.getElementById('root'))
 registerServiceWorker()
