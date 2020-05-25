d1525899000f3c2630220e006bcc1274b7a0a024
ibrahim <ibrahimsckn35@gmail.com>
2020-05-25 14:06:27 +0300

    Client side of the interest selection is finished
    Client side of the interest selection is implemented

--- client/src/components/interest/interest.js
+++ client/src/components/interest/interest.js
@@ -3,14 +3,28 @@ import { withTranslation } from 'react-i18next';
 import "./interest.css";
 
 class Interest extends Component {
-    handleClick(event) {
-        event.currentTarget.getElementsByClassName("checkmark")[0].style.display = "inline-block";
-        event.currentTarget.classList.add("checked")
+
+    handleClick = (event) => {
+        if (event.currentTarget.classList.contains("nonchecked")) {
+            event.currentTarget.getElementsByClassName("checkmark")[0].style.display = "inline-block";
+            event.currentTarget.classList.remove("nonchecked");
+            event.currentTarget.classList.add("checked");
+        }
+        else {
+            event.currentTarget.getElementsByClassName("checkmark")[0].style.display = "none";
+            event.currentTarget.classList.remove("checked");
+            event.currentTarget.classList.add("nonchecked");
+        }
+
+        if (this.props.onChange) {
+            this.props.onChange()
+        }
     }
+
     render() {
         const { t } = this.props;
         return (
-            <div className="column" onClick={this.handleClick}>
+            <div className="column nonchecked" onClick={this.handleClick}>
                 <div>
                     <span className="checkmark">
                         <div className="checkmark_circle"></div>
--- client/src/components/interest/interestSelection.js
+++ client/src/components/interest/interestSelection.js
@@ -4,22 +4,32 @@ import interests from "./sources";
 import { withTranslation } from "react-i18next";
 import "./interest";
 import TranslatedInterest from "./interest";
-
+import history from '../../services/history'
 
 
 class InterestSelection extends Component {
 
-  handleInterestClick(event){
-    let columnDiv = event.target.closest(".column")
-    columnDiv.style.background = "rgba(0,0,0,0.5)";
+  constructor(props) {
+    super(props);
+    this.state = {
+      numberOfTotalItems: 5,
+      numberOfRemainingItems: 5
+    }
   }
+
+  getNumberOfRemainingItems = () => {
+    let numberOfCheckedItems = document.querySelectorAll('.checked').length;
+    this.setState({ numberOfRemainingItems: this.state.numberOfTotalItems - numberOfCheckedItems })
+  }
+
   interestByCategories(category) {
     return interests.filter((x) => x.category == category).map((interest) => (
-        <TranslatedInterest
-          type={interest.type}
-          imgSource={interest.imgSource}
-          category={interest.category}
-        />
+      <TranslatedInterest
+        onChange={this.getNumberOfRemainingItems}
+        type={interest.type}
+        imgSource={interest.imgSource}
+        category={interest.category}
+      />
     ));
   }
 
@@ -27,7 +37,7 @@ class InterestSelection extends Component {
     const { category } = event.target.dataset;
 
     //active class is removed from all class
-    let allButtons = document.getElementsByClassName("btn");
+    let allButtons = document.getElementsByClassName("categoryButtons");
     for (let button of allButtons) {
       button.classList.remove("active");
     }
@@ -57,33 +67,51 @@ class InterestSelection extends Component {
     filteredElement.classList.add("show");
   }
 
+  redirectToHomePage() {
+    history.push('/');
+  }
 
   render() {
     const { t } = this.props;
     return (
-      <div className="container w-75 border">
-        <div id="myBtnContainer" style={{marginTop: "50px"}}>
-          <button data-category="all" className="btn active" onClick={this.handleFilter}>{t('InterestSelection.showall')}</button>
-          <button data-category="football" className="btn" onClick={this.handleFilter}>{t('InterestSelection.football')}</button>
-          <button data-category="basketball" className="btn" onClick={this.handleFilter}>{t('InterestSelection.basketball')}</button>
-          <button data-category="tennis" className="btn" onClick={this.handleFilter}>{t('InterestSelection.tennis')}</button>
-          <button data-category="motorsports" className="btn" onClick={this.handleFilter}>{t('InterestSelection.motorsports')}</button>
-          <button data-category="teams" className="btn" onClick={this.handleFilter}>{t('InterestSelection.teams')}</button>
+      <div className="container">
+        <div id="categoryTextDiv" className="border border-dark">
+          <span className="margin" style={{ fontSize: 19, fontWeight: "bold" }}>{t('InterestSelection.interestQuestion')}</span>
         </div>
-        <div id="football" className="row">
-          {this.interestByCategories("football")}
+        <div id="categoriesDiv" className="border border-dark">
+          <div id="categoryDiv" className="margin" >
+            <button data-category="all" className="categoryButtons active" onClick={this.handleFilter}>{t('InterestSelection.showall')}</button>
+            <button data-category="football" className="categoryButtons" onClick={this.handleFilter}>{t('InterestSelection.football')}</button>
+            <button data-category="basketball" className="categoryButtons" onClick={this.handleFilter}>{t('InterestSelection.basketball')}</button>
+            <button data-category="tennis" className="categoryButtons" onClick={this.handleFilter}>{t('InterestSelection.tennis')}</button>
+            <button data-category="motorsports" className="categoryButtons" onClick={this.handleFilter}>{t('InterestSelection.motorsports')}</button>
+            <button data-category="teams" className="categoryButtons" onClick={this.handleFilter}>{t('InterestSelection.teams')}</button>
+          </div>
+          <div id="football" className="row margin">
+            {this.interestByCategories("football")}
+          </div>
+          <div id="basketball" className="row margin">
+            {this.interestByCategories("basketball")}
+          </div>
+          <div id="tennis" className="row margin">
+            {this.interestByCategories("tennis")}
+          </div>
+          <div id="motorsports" className="row margin">
+            {this.interestByCategories("motorsports")}
+          </div>
+          <div id="teams" className="row margin">
+            {this.interestByCategories("teams")}
+          </div>
         </div>
-        <div id="basketball" className="row">
-          {this.interestByCategories("basketball")}
-        </div>
-        <div id="tennis" className="row">
-          {this.interestByCategories("tennis")}
-        </div>
-        <div id="motorsports" className="row">
-          {this.interestByCategories("motorsports")}
-        </div>
-        <div id="teams" className="row">
-          {this.interestByCategories("teams")}
+        <div id="submitButtonsDiv" className="border border-dark">
+          {
+            this.state.numberOfRemainingItems == 0 ?
+              (<button type="button" className="btn btn-primary floatRight" onClick={this.redirectToHomePage} >{t('InterestSelection.finish')}</button>)
+              :
+              (<button type="button" className="btn btn-primary disabled floatRight">{t('InterestSelection.continueText',
+                { numberOfRemainingItems: this.state.numberOfRemainingItems })}</button>)
+          }
+
         </div>
       </div>
     );
